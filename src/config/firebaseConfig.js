c4fba14f1f4f10e4c2cbde36bf9ca55a26a6e84b
Jillo Woche <jillo.abdulla@gmail.com>
2019-07-02 16:13:07 +0300

    configure firebase for the project

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAlhbergCOsmBoE5jHok16gOCZJefLIR70",
+    authDomain: "marioplan-f930a.firebaseapp.com",
+    databaseURL: "https://marioplan-f930a.firebaseio.com",
+    projectId: "marioplan-f930a",
+    storageBucket: "",
+    messagingSenderId: "705809517773",
+    appId: "1:705809517773:web:b0b371fe9fc919cc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.firestore().settings({ timestampsInSnapshots: true})
+
+  export default firebase;
