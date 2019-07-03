bc71e1f5593ecc1fc139de1558838fdf487e0432
Jillo Woche <jillo.abdulla@gmail.com>
2019-07-03 13:21:53 +0300

    set up signin with firebase

--- src/components/projects/ProjectsList.js
+++ src/components/projects/ProjectsList.js
@@ -8,7 +8,7 @@ const ProjectsList = ({projects}) => {
             { projects && projects.map(project => {
                 return (
                     <Link to={'/project/'+ project.id} key={project.id}>
-                        <ProjectSummary project={project} key={project.id} />
+                        <ProjectSummary project={project} />
                     </Link>
                 )
             })}
