54c4a530c73f48623e3c079e1dc4985d2b7fa942
Jillo Woche <jillo.abdulla@gmail.com>
2019-07-03 15:50:13 +0300

    check if auth is ready before displaying links

--- src/index.js
+++ src/index.js
@@ -15,13 +15,19 @@ const store = createStore(rootReducer,
     compose(
         applyMiddleware(thunk.withExtraArgument({ getFirebase, getFirestore })),
         reduxFirestore(firebaseConfig),
-        reactReduxFirebase(firebaseConfig)
+        reactReduxFirebase(firebaseConfig, {attachAuthIsReady: true})
         )
     );
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+// Render content only when auth is ready to avoid flicker of
+// non-logged in user SignedInLinks.
+store.firebaseAuthIsReady.then(() => {
+    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
