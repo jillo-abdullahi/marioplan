c18ace00cbeda93e923cf8087fdd7fb86319c1e7
Jillo Woche <jillo.abdulla@gmail.com>
2019-07-08 13:04:49 +0300

    handle signup and validation

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -20,3 +20,26 @@ export const signOut = () => {
         })
     }
 }
+
+export const signUp = (newUser) => {
+    return (dispatch, getState, { getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+        ).then((response) => {
+            return firestore.collection('users').doc(response.user.uid).set({
+                firstname: newUser.firstname,
+                lastname: newUser.lastname,
+                initials: newUser.firstname[0]+ newUser.lastname[0]
+            });
+            }).then(() => {
+                dispatch({ type: 'SIGNUP_SUCCESS'})
+            }).catch((err) => {
+                dispatch({ type: 'SIGNUP_ERROR', err})
+        })
+    }
+
+}
