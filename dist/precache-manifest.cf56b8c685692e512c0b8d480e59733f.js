3125f744d3a3652834276778e82bdfcdefff551c
Jillo Woche <jillo.abdulla@gmail.com>
2019-07-09 15:00:58 +0300

    deploy to firebase

---
+++ dist/precache-manifest.cf56b8c685692e512c0b8d480e59733f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38fef1323a2e45d1910baf7159584144",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ec697a47d00d94e47d",
+    "url": "/static/css/main.e7da2018.chunk.css"
+  },
+  {
+    "revision": "d4f06bb6bd9474d90fb9",
+    "url": "/static/js/2.6eaf7d7c.chunk.js"
+  },
+  {
+    "revision": "76ec697a47d00d94e47d",
+    "url": "/static/js/main.59ab2e48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
